12b4869ad962f139ec6433e22318d6cd7eace401
mmaia <maia.marcos@gmail.com>
2025-10-18 15:42:50 +0200

    improved variable name for clarity

--- composeApp/src/commonMain/kotlin/com/mgroo/gtsimhub/Oauth2Helper.kt
+++ composeApp/src/commonMain/kotlin/com/mgroo/gtsimhub/Oauth2Helper.kt
@@ -5,13 +5,13 @@ import org.kotlincrypto.random.CryptoRand
 import kotlin.io.encoding.Base64
 
 val discordAuth = mutableStateOf(false)
-const val clientId = "client_id=1426838693150326825"
+const val discordClientId = "client_id=1426838693150326825"
 const val responseType = "response_type=code"
 const val redirectUri = "redirect_uri=http%3A%2F%2Flocalhost%3A8888%2Fapi%2Fauth%2Fdiscord%2Fredirect"
 const val scope = "scope=identify+email"
 fun discordAuthUrl(): String {
     val state = generateSecureOAuthState()
-    val discordAuthUrl = "https://discord.com/oauth2/authorize?$clientId&$responseType&$redirectUri&$scope&state=$state"
+    val discordAuthUrl = "https://discord.com/oauth2/authorize?$discordClientId&$responseType&$redirectUri&$scope&state=$state"
     return discordAuthUrl
 }
 
