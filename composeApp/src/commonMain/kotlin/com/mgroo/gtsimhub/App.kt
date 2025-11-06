f8c21eeb909dc7d92c9970b9ec8600d76cfef6d6
mmaia <maia.marcos@gmail.com>
2025-11-06 15:40:24 +0100

    Moved HomeScreen to it's own file

--- composeApp/src/commonMain/kotlin/com/mgroo/gtsimhub/App.kt
+++ composeApp/src/commonMain/kotlin/com/mgroo/gtsimhub/App.kt
@@ -1,81 +1,13 @@
 package com.mgroo.gtsimhub
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
-import gt_sim_hub.composeapp.generated.resources.Discord_Symbol_Blurple
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import gt_sim_hub.composeapp.generated.resources.Res
-import gt_sim_hub.composeapp.generated.resources.bkground_blurred
-import gt_sim_hub.composeapp.generated.resources.gt_league_hub_logo
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         HomeScreen()
-    }
-}
-
-@Composable
-fun HomeScreen() {
-
-    if(discordAuth.value) {
-        val state = generateSecureOAuthState()
-        openUrl(discordAuthUrl(state))
-        saveState(state)
-        discordAuth.value = false
-        // Navigate to AuthScreen
-    }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(Res.drawable.bkground_blurred),
-            contentDescription = "Background",
-            contentScale = ContentScale.Crop, // This scales the image to fill the screen, cropping excess
-            modifier = Modifier.fillMaxSize()
-        )
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text("Welcome, let's go race!",
-                style = MaterialTheme.typography.headlineLarge,
-                color = Color.White)
-            Spacer(modifier = Modifier.height(32.dp))
-            Image(
-                painter = painterResource(Res.drawable.gt_league_hub_logo),
-                contentDescription = "GT League Hub Logo",
-                modifier = Modifier.size(150.dp)
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-            Button(onClick = { discordAuth.value = true } ) {
-                Icon(painterResource(Res.drawable.Discord_Symbol_Blurple), contentDescription = "Discord Logo", modifier = Modifier.size(24.dp))
-                Spacer(modifier = Modifier.width(8.dp))
-                Text("Login with Discord")
-            }
-        }
     }
 }
